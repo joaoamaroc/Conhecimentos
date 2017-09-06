8fb3d5b1f9940ef4ff77fd4f3fab022ea893dc34
João Pedro Amaro <joao.amaroc@gmail.com>
2017-09-06 19:03:42 -0300

    Add files via upload

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,1 @@
 insert into Person(NOME, IDADE) values('bla',21);
-
-insert into Knowledges(LEVEL) values('N');
-insert into Knowledges(LEVEL) values('K');
-insert into Knowledges(LEVEL) values('B');
-insert into Knowledges(LEVEL) values('I');
-insert into Knowledges(LEVEL) values('S');
